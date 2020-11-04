4178697e2800cec5446db74d3b5496fd563ddd5b
Rutuj Bhakre <rbhakre99@gmail.com>
2020-11-04 12:06:14 +0530

    inherited

--- inheritance.php
+++ inheritance.php
@@ -13,11 +13,23 @@ class Fruit{
 	}
 
 }
+class Apple extends Fruit{
+	// public function get_color(){
+	// 	return $this->color;                   //can return color if the property is protected or public
+	// }
+}
+
+
 
 $mango= new Fruit('mango','yellow');
 echo $mango->get_name();
+echo "<br>";
 
-$mango->color='red'; //can change color only if the property are public
-echo $mango->get_name(); 
+// $mango->color='red';                           //can change color only if the properties are public
+// echo $mango->get_name(); 
 
+
+$apple = new Apple('apple','red');
+echo $apple->get_color();
+echo $apple->get_name();
 ?>
